117736c78938fffc7a09fbe5b9cb2c8bd26153bc
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-10-10 12:29:41 +0200

    Update the function call syntax, yet again

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from lexer import lexer
 
-lexer('owo stay name = (nya? "What\'s your name?")\nmeow "Hello"..name.."!"')
+lexer('owo stay name = nya?("What\'s your name?")\nmeow("Hello"..name.."!")')
 # program:
 """
-owo stay name = (nya? "What's your name?")          name = input("What's your name?")
-meow "Hello "..name.."!"                            print(f'Hello {name}!')
+owo stay name = nya?("What's your name?")          name = input("What's your name?")
+meow("Hello "..name.."!")                          print(f'Hello {name}!')
 """
