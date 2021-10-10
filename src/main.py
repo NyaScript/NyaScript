0664535e8303e2ce559efa8a4081d7cf05b960ec
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-10-10 12:05:44 +0200

    New test program

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 from lexer import lexer
 
-lexer("")
+lexer('owo stay name = (nya? "What\'s your name?")\nmeow "Hello"..name.."!"')
+# program:
+"""
+owo stay name = (nya? "What's your name?")          name = input("What's your name?")
+meow "Hello "..name.."!"                            print(f'Hello {name}!')
+"""
